3efb12a553742d4a7b3a0caf75ba39a9ae5e945e
Sarah Pearson <sarahp@me.com>
2014-10-28 16:46:28 -0500

    menu has background image and start button

--- public/js/menu.js
+++ public/js/menu.js
@@ -5,16 +5,27 @@ var menu = (function(){
     update: update
   };
 
+//var button;
+
   function preload(){
+    game.load.image('bg', '/assets/background.png');
+    //game.load.spritesheet('button', '/assets/button_sprite_sheet.png', 193, 71);
+    game.load.spritesheet('button', '/assets/START3.png');
 
   }
 
   function create(){
 
+    game.add.sprite('0', '0', 'bg');
+    button = game.add.button(game.world.centerX - 140, game.world.centerY - 140, 'button', startClick);
   }
 
   function update(){
 
+  }
+
+  function startClick () {
+    this.game.state.start('lvl1');
   }
 
   return o;
