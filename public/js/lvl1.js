5abe99e40409087d0f4a334c42497c0f9200d115
Adam Barnhard <admbarn@gmail.com>
2014-10-29 16:59:02 -0500

    added stuff

--- public/js/lvl1.js
+++ public/js/lvl1.js
@@ -110,6 +110,7 @@
     }
 
     if(Math.abs(player.x - (tile * 212)) <= 20 && Math.abs(player.y - (4 * tile)) >= 32){
+      player.destroy();
       game.state.start('lvl2');
     }
     var screenHeight = 480;
